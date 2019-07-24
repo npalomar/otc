86a163c7a62c90afa8c1b213fd31d234a89e407e
npalomar <n.palomar@yahoo.com>
2019-07-24 17:39:14 +1200

    test

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Olya's Travel Connection<title>
+    <link rel="stylesheet" href="/css/custom.css">
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
